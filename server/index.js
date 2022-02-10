348d573c992d9f84c623fcbb4f801ead6fce6ff1
geepara <gpara1697@gmail.com>
2022-02-10 14:29:16 -0500

    cors config

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const pairs = require("./routes/pairs");
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Gee URL API!");
